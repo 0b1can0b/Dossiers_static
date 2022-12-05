bb37da10411f09ca4ba89fa71cf5032ed9b026ef
0b1can0b <0b1can0b21@gmail.com>
2022-12-06 05:21:43 +0530

    c

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-$('.button').popup({
-  on: 'click',
-  position: 'bottom right',
-  inline: false,
+$(".tool-contacts").popup({
+  on: "click",
+  position: "bottom right",
+});
+
+$(".tool-settings").popup({
+  on: "click",
+  position: "bottom right",
 });
 
 // $('.menu .item').tab();
@@ -10,11 +14,27 @@ $('.button').popup({
 //   context: $('.contacts-popup-tabs'),
 // });
 
-$('.contacts-popup-tabs a').click(function () {
-  $('.contacts-popup-tabs a.active').removeClass('active');
-  $('.contacts-popup-tabs-content.active').removeClass('active');
-  $(this).addClass('active');
+$(".contacts-popup-tabs a").click(function () {
+  $(".contacts-popup-tabs a.active").removeClass("active");
+  $(".contacts-popup-tabs-content.active").removeClass("active");
+  $(this).addClass("active");
   $(
-    `.contacts-popup-tabs-content[data-tab="${$(this).attr('data-tab')}"]`
-  ).addClass('active');
+    `.contacts-popup-tabs-content[data-tab="${$(this).attr("data-tab")}"]`
+  ).addClass("active");
 });
+
+$(".ui.dropdown").dropdown();
+
+$(".ui.accordion").accordion();
+
+$(".header-nav-tabs > *").click(function () {
+  $(".header-nav-tabs > *").removeClass("active");
+  $(this).addClass("active");
+});
+
+$(".filter-buttons > *").click(function () {
+  $(".filter-buttons > *").removeClass("active");
+  $(this).addClass("active");
+});
+
+$(".dossiers-button").addClass("active");
